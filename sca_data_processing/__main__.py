c5936dba8256ee56404aa4a11e00cdaf969cb048
femigr <femigr@gmail.com>
2021-11-13 19:00:19 +0100

    changed the basic 'interface'

--- sca_data_processing/__main__.py
+++ sca_data_processing/__main__.py
@@ -2,30 +2,34 @@ import sca_data_processing.api as sca
 import argparse
 
 parser = argparse.ArgumentParser(description='Create an SCA dataset.')
+parser.add_argument('command', metavar='command', type=str,
+                    help='the command: "none", "get_src_paths"')
 parser.add_argument('path', metavar='path', type=str,
                     help='the path of the base folder for the data')
 
 args = parser.parse_args()
 
+if args.command == "get_src_paths":
+    paths = sca.get_src_file_paths("../sca-data/rolldice")
+    for path in paths:
+        print(path)
 
-print(args.path)
-
-print(sca.get_src_file_paths("../sca-data/rolldice"))
-
-
-sca_data = sca.create_dataset(args.path)
-print(sca_data)
+elif args.command == "none":
+    sca_data = sca.create_dataset(args.path)
+    print(sca_data)
 
 
-print(len(sca_data.new_issues))
-print(len(sca_data.old_issues))
-# severities = {'information', 'error', 'recommendation', 'warning'}
+    print(len(sca_data.new_issues))
+    print(len(sca_data.old_issues))
+    # severities are {'information', 'error', 'recommendation', 'warning'}
 
-severities = {'information': [], 'error': [], 'recommendation': [], 'warning': []}
+    severities = {'information': [], 'error': [], 'recommendation': [], 'warning': []}
 
-for issue in sca_data.old_issues:
-    severities[issue.severity].append(issue)
+    for issue in sca_data.old_issues:
+        severities[issue.severity].append(issue)
 
-for severity in severities:
-    print(severity + " " + str(len(severities[severity])))
+    for severity in severities:
+        print(severity + " " + str(len(severities[severity])))
 
+else:
+    print("Unknown command: " + args.command)
